3c022cc4e71f95ef31c3713fbfb9b37abee198b6
James Volmert <james.volmert@gmail.com>
2024-10-19 18:51:38 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ChatBot from "./components/ChatBot/ChatBot";
+function App() {
+  return (
+    <div className="container">
+      <h2>React app</h2>
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBot/ChatBot.jsx
@@ -0,0 +1,13 @@
+import "./ChatBot.style.css";
+
+const ChatBot = () => {
+  return (
+    <div>
+      <h2 className="start-page">
+        <button className="start-page-btn">Chat AI</button>
+      </h2>
+    </div>
+  );
+};
+
+export default ChatBot;
